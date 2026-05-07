e2b14215dc80a7fba15539a8a9136efe2b318a07
25023182-ux <25023182@vnu.edu.vn>
2026-05-07 17:33:03 +0700

    Update HelloMaven.java

--- src/main/java/github/tobsef/demo/HelloMaven.java
+++ src/main/java/github/tobsef/demo/HelloMaven.java
@@ -2,6 +2,6 @@ package github.tobsef.demo;
 
 public class HelloMaven {
     public static void run() {
-        System.out.println("Hello Maven!");
+        System.out.println(" Hello Maven! ");
     }
 }
